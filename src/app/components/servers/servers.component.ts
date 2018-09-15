dba5224f02b549cb7b5d466966060c7952aaa72f
Przemysław Jamorski <pjamorski@pl.sii.eu>
2018-09-15 16:23:36 +0200

    Server project I step finished

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -10,18 +10,31 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = "No server was created!";
   serverName: string = "";
   serverCreated: boolean = false;
+  servers: Array<string>;
+
+  offlineServersCount: number;
+  onlineServersCount: number;
+
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = false;
     }, 2000);
+
+    this.servers = new Array<string>();
+    this.servers.push("Test Server 1");
+    this.servers.push("Test Server 2");
+
+    this.onlineServersCount = 0;
+    this.offlineServersCount = 0;
   }
 
   ngOnInit() {}
 
   onCreateServer(): void {
-    this.serverCreationStatus =
-      "Server was created! Name is " + this.serverName;
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
     this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: any): void {
