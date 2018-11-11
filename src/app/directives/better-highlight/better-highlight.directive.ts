f83e2d21f4b8231aa044140a4e7c76518dce7ec2
Przemysław Jamorski <pjamorski@pl.sii.eu>
2018-11-11 19:41:39 +0100

    Attribute directives - finished.

--- src/app/directives/better-highlight/better-highlight.directive.ts
+++ src/app/directives/better-highlight/better-highlight.directive.ts
@@ -1,17 +1,49 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  @Input() clickedColor: string = 'green';
+  @Input() data: any;
+
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
   constructor(
     private renderer: Renderer2,
     private elementRef: ElementRef
-    ) { }
+    ) {}
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#e6ecff');
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#0000b3');
+    this.backgroundColor = this.defaultColor;
   }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event){
+    console.log('@HostListener("mouseOver")');
+    console.log(this.data);
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event){
+    console.log('@HostListener("mouseLeave")');
+    console.log(this.data);
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('click') mouseClick(eventData: Event){
+    console.log('@HostListener("click")');
+    console.log(this.data);
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'byellowlue');
+
+    this.backgroundColor = this.clickedColor;
+  }
+
 
 }
